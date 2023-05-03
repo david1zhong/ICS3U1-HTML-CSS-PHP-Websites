1b2614162324d63dce298122ab6f9297c62beb7e
david1zhong <127060667+david1zhong@users.noreply.github.com>
2023-05-02 21:21:45 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php if (!isset($_SESSION)) { session_start(); } ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">	
+	<title>David Zhong: ICS3UO-02</title>	
+	
+	<meta name="description" content="David's page for all ICS3UO related work">
+	<meta name="author" content="David Zhong">	
+	
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100&family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="styles.css">
+
+</style>
+
+</head>
+<body>
+	
+	<div class="activities_2-3">
+		<h1>Activities 2.3</h1>
+		<a href="choose-your-adventure.php">Activity 2.3c [CYOA]</a><br /><br />
+	</div>
+
+	<br><br>
+
+	<div class="activities_2-4">
+		<h1>Activities 2.4</h1>
+		<a href="contact-form.php">Activity 2.4C [CONTACT FORM]</a><br />
+		<a href="activity-dice-game.php">Activity 2.4D [DICE GAME]</a><br /><br />
+	</div>
+
+	<br><br>
+ 
+	<div class="activities_2-7">
+		<h1>Activities 2.7</h1>
+		<a href="activity-2-7-acceleration-of-gravity.php">Activity 2.7B [GRAVITY ARRAY]</a><br />
+		<a href="activity-2-7-binary-to-decimal.php">Activity 2.7C [BINARY]</a><br />
+	</div>
+ 
+	<br><br>
+
+	<div class="activities_fpt">
+		<h1>FPT</h1>
+		<a href="perftask-rock-paper-scissors.php">TASK #1 [Rock Paper Scissors]</a><br>
+		<a href="perftask-rock-paper-scissors.php">TASK #2 []</a><br>
+		<a href="perftask-rock-paper-scissors.php">TASK #3 []</a><br>
+		<a href="perftask-rock-paper-scissors.php">TASK #4 []</a><br>
+	</div>
+
+	<!-- end main content -->
+	
+	<!-- turn work in widget -->
+	<?php include $_SERVER['DOCUMENT_ROOT'] . "/marking-rubric/turn-work-in.inc.php"; ?>
+</body>
+</html>
+
