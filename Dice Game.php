a03b623094735a5ac8f69c5086c2c04a26d80198
david1zhong <127060667+david1zhong@users.noreply.github.com>
2025-08-18 20:13:36 -0400

    Update and rename Dice Game to Dice Game.php

--- Dice Game.php
+++ Dice Game.php
@@ -1,4 +1,3 @@
-<?php if (!isset($_SESSION)) { session_start(); } ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -118,5 +117,4 @@
 
 
 </body>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/marking-rubric/turn-work-in.inc.php"; ?>
 </html>
